e02cda80994794e5c6a423258f10f48f399e11de
moixeta <113198118+KatiaGonGon@users.noreply.github.com>
2022-09-28 23:05:20 +0200

    First commit from cli by Katia

---
+++ src/main/java/com/katia/form001/Form001Application.java
@@ -0,0 +1,13 @@
+package com.katia.form001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Form001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/katia/form001/Form001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katia.form001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Form001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
